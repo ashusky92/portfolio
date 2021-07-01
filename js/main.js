2388bb1cab7ebe31d5656c666e18e5f2c51b184c
alex.husky <alexanderhusky@gmail.com>
2021-06-30 18:15:55 -0700

    Added a few sections and a form.  I also updated the overall styling.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+
+// Adds/Removes acviated class to navigation, depending on scroll location
 $(window).on('scroll', function(){
   var scrollPos = $(this).scrollTop();
-  if(scrollPos > 520){
-    $('nav').addClass('activated');
-    $('nav div.nav-content h2.nav-title').slideDown();
+  if(scrollPos > 885){
+    $('nav').slideDown();
   } else {
-    $('nav').removeClass('activated');
-    $('nav div.nav-content h2.nav-title').slideUp();
+    $('nav').slideUp();
   }
+});
+
+$('section.multi-section div.buttons-container button.multi-btn').on('click', function(){
+	var sectTarget = $(this).attr('sect-target');
+  $(this).addClass('active').siblings().removeClass('active');
+  $(this).parent().parent().find('div.content-container div#' + sectTarget).addClass('active').siblings().removeClass('active');
 });
