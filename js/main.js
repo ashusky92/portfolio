dfcb8d490ebbd625ea1cccc6fb1c00051fe4a9f5
alex.husky <alexanderhusky@gmail.com>
2021-07-03 01:07:31 -0700

    Fixed drop-down nav issue

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@
 */
 
 // Find scroll location of header navigation
-var el = $('div.header-nav');
+var el = $('div.header-text');
 var navLoc = el.position().top + el.offset().top + el.outerHeight(true);
+
+console.log(`Navigation Location: ${navLoc}`);
 
 // Adds functionality to elements through website, depending on scroll position
 $(window).on('scroll', function(){
   var scrollPos = $(this).scrollTop();
   if($(window).width() > 992){
-    if(scrollPos > navLoc){
+    if(scrollPos > (navLoc - 20)){
       $('nav').slideDown();
     } else {
       $('nav').slideUp();
